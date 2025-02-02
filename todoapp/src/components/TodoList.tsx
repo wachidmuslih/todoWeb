dc89062121adca1f8aaedc3cc924278ae17b3b35
wachidmuslih <wachidmslh@gmail.com>
2025-02-02 07:17:11 +0700

    style: update TodoList item layout for better alignment and consistency

--- todoapp/src/components/TodoList.tsx
+++ todoapp/src/components/TodoList.tsx
@@ -5,15 +5,24 @@ const TodoList = () => {
         <div className="w-[400px]">
             <h2 className="text-xl font-bold mb-4 text-gray-800">Daftar Todo</h2>
             <ul className="flex flex-col gap-y-2 font-archivo font-normal text-gray-800 text-base">
-                <li className="flex p-4 bg-gray-50 border-[1px] border-[#EEEEEE] border-solid rounded-lg hover:bg-gray-100 ">
+                <li className="flex justify-between p-4 bg-gray-50 border-[1px] border-[#EEEEEE] border-solid rounded-lg hover:bg-gray-100 ">
                     <span className="mr-2">Ini catatan yoyiggiyo</span>
-                    <button className="text-red-500">
+                    <button className="text-red-500 flex items-center gap-x-2">
                         <IconTrash size={20} color="#fb2c36" />
-                        <span>Hapus</span>
+                        {/* <span>Hapus</span> */}
                     </button>
                 </li>
-                <li className="flex p-4 bg-gray-50 border-[1px] border-[#EEEEEE] border-solid rounded-lg hover:bg-gray-100 ">Ini catatan yoyiggiyo</li>
+                <li className="flex justify-between p-4 bg-gray-50 border-[1px] border-[#EEEEEE] border-solid rounded-lg hover:bg-gray-100 ">
+                    <span className="mr-2">Ini catatan yoyiggiyo</span>
+                    <button className="text-red-500 flex items-center gap-x-2">
+                        <IconTrash size={20} color="#fb2c36" />
+                        {/* <span>Hapus</span> */}
+                    </button>
+                </li>
+                
             </ul>
+            
+
         </div>
     )
 }
