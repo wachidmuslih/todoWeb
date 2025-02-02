d32099e73efe582cc53f2b3dc8c10eb89b26d980
wachidmuslih <wachidmslh@gmail.com>
2025-02-02 07:11:57 +0700

    feat: add delete button to TodoList items with trash icon

--- todoapp/src/components/TodoList.tsx
+++ todoapp/src/components/TodoList.tsx
@@ -1,14 +1,21 @@
+import { IconTrash } from "@tabler/icons-react"
 
 const TodoList = () => {
-  return (
-    <div className="w-[400px]">
-        <h2 className="text-xl font-bold mb-4 text-gray-800">Daftar Todo</h2>
-        <ul className="flex flex-col gap-y-2 font-archivo font-normal text-gray-800 text-base">
-            <li className="flex p-4 bg-gray-50 border-[1px] border-[#EEEEEE] border-solid rounded-lg hover:bg-gray-100 ">Ini catatan yoyiggiyo</li>
-            <li className="flex p-4 bg-gray-50 border-[1px] border-[#EEEEEE] border-solid rounded-lg hover:bg-gray-100 ">Ini catatan yoyiggiyo</li>
-        </ul>
-    </div>
-  )
+    return (
+        <div className="w-[400px]">
+            <h2 className="text-xl font-bold mb-4 text-gray-800">Daftar Todo</h2>
+            <ul className="flex flex-col gap-y-2 font-archivo font-normal text-gray-800 text-base">
+                <li className="flex p-4 bg-gray-50 border-[1px] border-[#EEEEEE] border-solid rounded-lg hover:bg-gray-100 ">
+                    <span className="mr-2">Ini catatan yoyiggiyo</span>
+                    <button className="text-red-500">
+                        <IconTrash size={20} color="#fb2c36" />
+                        <span>Hapus</span>
+                    </button>
+                </li>
+                <li className="flex p-4 bg-gray-50 border-[1px] border-[#EEEEEE] border-solid rounded-lg hover:bg-gray-100 ">Ini catatan yoyiggiyo</li>
+            </ul>
+        </div>
+    )
 }
 
 export default TodoList
